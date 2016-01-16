28b607ce97a0a9e4cf843363ac70133ffdb93f0d
flickerinhouse <rtemelkovski@yahoo.com>
2016-01-16 10:20:34 -0500

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deltahacksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the deltahacksApp
+ */
+angular.module('deltahacksApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deltahacksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deltahacksApp
+ */
+angular.module('deltahacksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
