c9478ffdfa262f44acacf17cb14abf0197d302ef
Pedro Oliveira <olivepjm@mcmaster.ca>
2016-01-17 12:22:39 -0500

    Done

--- angular/app/scripts/controllers/patients.js
+++ angular/app/scripts/controllers/patients.js
@@ -45,6 +45,7 @@ angular.module('deltahacksApp')
     		$scope.people[i][7].ename = response.data.ename;
     		$scope.people[i][7].enumber = response.data.enumber;
     		$scope.people[i][7].phone = response.data.number;
+        $scope.people[i][7].gcmToken = response.data.gcmToken;
 
     	}, function errorCallback(response){
     		console.log(response);
@@ -52,24 +53,26 @@ angular.module('deltahacksApp')
     }
     $scope.submit = function(obj, index, person){
       var time = [obj.hour, obj.minute, obj.ampm].join(":")
-      console.log($scope.people[index][1]);
-      console.log(obj.frequency);
-      console.log(time);
-      console.log(obj.date);
-      console.log(obj.message);
+      // console.log($scope.people[index]);
+      // console.log(obj.frequency);
+      // console.log(time);
+      // console.log(obj.date);
+      // console.log(obj.message);
       obj.hour = parseInt(obj.hour) + 5;
       time = obj.hour + ':'+ obj.minute;
-      console.log(time);
+      // console.log(time);
       $http({
         'method' : 'POST',
         'url' : 'http://172.17.148.27:3000/notification',
         'header' : 'application/json',
         'data': {
-          'gcmToken' : $scope.people[index][1], //people[index].gcmtoken
+          'gcmToken' : $scope.people[index][7].gcmToken, //people[index].gcmtoken
           'freq' : obj.frequency, //frequency
           't' : time, //time
           'd': obj.date, //date
-          'message' : obj.message //message
+          'message' : obj.message, //message
+          'firstname' : $scope.people[index][1],
+          'lastname' : $scope.people[index][2]
         }
       }).then(function successCallback(response) {
         console.log(response);
@@ -79,6 +82,6 @@ angular.module('deltahacksApp')
       }, function errorCallback(response) {
         console.log(response);
       });
-      console.log("SUBMIT " + index)
+      // console.log("SUBMIT " + index)
     }
   });
